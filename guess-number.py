3c079aa50a90b036fc5e862bbdf26eee783693f2
myKalling <owner@mykalling.net>
2026-04-17 08:53:22 -0700

    FizzBuzz and Number

---
+++ guess-number.py
@@ -0,0 +1,23 @@
+import random
+
+
+secret_number = 1
+print(secret_number) # For Testing purposes, you can comment this out in production
+
+guess = None
+
+while guess != secret_number:
+    guess_input = input("Guess the number between 1 and 10: ").strip()
+
+    if not guess_input.isdigit():
+        print("Invalid input. Please enter a number between 1 and 10.")
+        continue
+
+    guess = int(guess_input)
+
+    if guess < secret_number:
+        print("Too low! Try again.")
+    elif guess > secret_number:
+        print("Too high! Try again.")
+    else:
+        print("Congratulations! You've guessed the number!")
